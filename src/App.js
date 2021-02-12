50d930f7fcd36e31d576493b32bbda9a7a93f720
Jacob Ruiz <jacobruizdesign@gmail.com>
2021-02-12 13:42:38 +1000

    move everything into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import TextInput from './components/TextInput/TextInput';
+import React, { useState } from 'react';
 import './styles.css';
+
+function TextInput({ type = 'text', label }) {
+  const [value, setValue] = useState('');
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <div className="input-container">
+      <input type={type} value={value} onChange={handleChange} />
+      <label className={value && 'filled'} htmlFor={name}>
+        {label}
+      </label>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
-      <TextInput label="Label long very longgggg" />
+      <form>
+        <TextInput label="Username" />
+        <TextInput label="Password" type="password" />
+      </form>
     </div>
   );
 }
